c1f68eb21efa303a8ca0add715c3ece4ff413300
Tony Willsung <tonywillsung@exotic-mac.local>
2016-10-14 22:17:42 +0800

    Initial Commit

---
+++ Grammer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Grammer
+//
+//  Created by Tony Willsung on 2016/10/14.
+//  Copyright © 2016年 Tony Willsung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
